a04ca2d4ae51d03382673f6707a98e0eb0d82486
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-01-13 12:47:44 -0800

    Added components directory

--- dementiaid/src/App.js
+++ dementiaid/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter as Router, Switch, Redirect, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
