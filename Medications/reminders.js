d47cc673d7498abea9cbd1e774feaff037032478
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2018-01-13 15:19:12 -0800

    Added some functionality

---
+++ Medications/reminders.js
@@ -0,0 +1,8 @@
+let upcoming = document.querySelector("#upcoming");
+upcoming.addEventListener("click", function() {
+    upcoming.classList.add("border", "border-success");
+});
+
+function markAsTaken() {
+
+}
